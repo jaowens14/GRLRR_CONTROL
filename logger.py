a37b5b380e6bf2636142f122038722b50d8cf1a9
jaowens14 <jaowens14@protonmail.com>
2024-08-06 11:59:14 -0600

    added some setup scripts

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import logging
 
 grlrr_log = logging.getLogger(__name__)
 logging.basicConfig(filename='grlrr.log', 
-                    level=logging.INFO, 
+                    level=logging.DEBUG, 
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
