3d63a9691fa1c566842c86f0f43a8d1782801f3b
jaowens14 <jaowens14@protonmail.com>
2024-08-06 11:59:14 -0600

    added some setup scripts

--- grlrr_run.sh
+++ grlrr_run.sh
@@ -7,5 +7,9 @@ cd Documents/Projects/grlrr/
 # activate enviroment
 source .venv/bin/activate
 
+#store the proces id:
+echo 'Process ID: ' >>grlrr.log
+echo $$ >>grlrr.log
+
 # start the main program
 python main.py
