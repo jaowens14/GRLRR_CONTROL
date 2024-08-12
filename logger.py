fdfd6f1ae7f2fdb7bd9ff39ab7223ad61d326916
jaowens14 <jaowens14@protonmail.com>
2024-08-12 13:45:35 -0600

    updated logger

--- logger.py
+++ logger.py
@@ -1,9 +1,14 @@
 #logger.py
 
 import logging
+import os
+from datetime import datetime
+
+log_file_name = os.path.join("logs", ("grlrr_" + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ".log").replace(" ", "_"))
 
 grlrr_log = logging.getLogger(__name__)
-logging.basicConfig(filename='grlrr.log', 
+
+logging.basicConfig(filename=log_file_name, 
                     level=logging.INFO, 
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
