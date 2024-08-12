583849c86245531543ab801adc63e37a7324396a
jaowens14 <jaowens14@protonmail.com>
2024-08-12 08:59:56 -0600

    updated the formating for the log_server

--- log_server.py
+++ log_server.py
@@ -1,13 +1,14 @@
-import asyncio
+from logger import grlrr_log
 from aiohttp import web
 
 async def handle(request):
     f_served = open('grlrr.log','rb')
-    f_content = f_served.read()
+    f_content = f_served.read().decode('utf-8')
     f_served.close()
     return web.Response(text=f_content)
 
 async def run_log_server():
+    grlrr_log.info("log server started")
     app = web.Application()
     app.add_routes([web.get('/', handle)])
     runner = web.AppRunner(app)
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 import communications
-import log_server
 
 if __name__ == "__main__":
     PID = str(os.getppid())
     with open('grlrr.pid', 'w') as file:
         file.write(PID)
         file.close()
-    log_server.main()
     communications.main()
