01e793271eca3f3d1259c3afa91672dda746fa00
jaowens14 <jaowens14@protonmail.com>
2024-09-27 10:25:33 -0600

    upload

--- camera_server.py
+++ camera_server.py
@@ -317,8 +317,8 @@ async def run_camera_server():
 
             data = str(base64.b64encode(encoded))
 
-            #await image_queue.put(data[2:len(data)-1])
-            #image_queue.put("test")
+            await image_queue.put(data[2:len(data)-1])
+            image_queue.put("test")
             grlrr_log.info("robot angle: "+str(robot_angle))
             await angle_queue.put(robot_angle)
             
